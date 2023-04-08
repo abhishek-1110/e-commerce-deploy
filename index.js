ef320a344c8e52ef6a7fa693c611174710ffedba
Abhishek <abhishkbhi@gmail.com>
2023-04-08 16:03:47 +0530

    removed dir

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import { v4 as uuid} from "uuid";
 import * as path from "path";
-import { fileURLToPath } from 'url';
-
 // intializing
 
 // creating server
@@ -29,8 +27,6 @@ Connection();
 
 if (process.env.NODE_ENV === 'production') {
   
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
 
   app.use(express.static(path.join(__dirname, 'client', 'build')))
 
