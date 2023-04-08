160d09184244a71c69ab26b5a816eb5a26a4d4cb
Abhishek <abhishkbhi@gmail.com>
2023-04-08 18:30:55 +0530

    modified

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ console.log(__dirname);
 if (process.env.NODE_ENV === 'production') {
   
 
-  app.use(express.static(path.join(__dirname, 'dist')))
+  app.use(express.static(path.join(__dirname, 'client', 'build')))
 
   app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'dist'))
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
   });
 }
 
