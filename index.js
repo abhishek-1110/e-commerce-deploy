4034e2cf7ccb1a94aef8d48e6a0cc81f87d9f4e6
Abhishek <abhishkbhi@gmail.com>
2023-06-03 16:50:17 +0530

    gpt code

--- index.js
+++ index.js
@@ -1,61 +1,55 @@
-// express acts as a server
 import express from "express";
 import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import path from "path";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { v4 as uuid } from "uuid";
 import Connection from "./database/db.js";
 import DefaultData from "./default.js";
 import router from "./routes/route.js";
-import cors from "cors";
-import bodyParser from "body-parser";
-import { v4 as uuid} from "uuid";
-import { fileURLToPath } from "url";
-import path from 'path';
-
-// intializing
-
-// creating server
-// three arguments - port, callback function
 
 const app = express();
-
 dotenv.config();
-app.use(cors());
-app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
-const PORT = process.env.PORT || 8000;
 
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connect to the database
 Connection();
 
-// const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const __dirname = path.resolve();
+// Serve static files from the client/build directory
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-// console.log(__dirname);
+// API routes
 app.use("/", router);
 
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
-  // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
+// Serve the React app for all other routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
-
-app.listen(PORT, () =>
-  console.log(`Server is running successfully on Port ${PORT}`)
-);
+// Start the server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running successfully on Port ${PORT}`);
+});
 
 // DefaultData();
 
-export let paytmMerchantkey = process.env.PAYTM_MERCHANT_KEY;
-export let paytmParams = {};
-paytmParams['MID'] = process.env.PAYTM_MID;
-paytmParams['WEBSITE'] = process.env.PAYTM_WEBSITE;
-paytmParams['CHANNEL_ID'] = process.env.PAYTM_CHANNEL_ID;
-paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID;
-paytmParams['ORDER_ID'] = uuid();
-paytmParams['TXN_AMOUNT'] = '100'
-paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID;
-paytmParams['CALLBACK_URL'] = 'http://localhost:8000/callback';
-paytmParams['EMAIL'] = 'abhishkbhi@gmail.com';
-paytmParams['MOBILE_NO'] = '1234567890';
+export const paytmMerchantkey = process.env.PAYTM_MERCHANT_KEY;
+export const paytmParams = {
+  MID: process.env.PAYTM_MID,
+  WEBSITE: process.env.PAYTM_WEBSITE,
+  CHANNEL_ID: process.env.PAYTM_CHANNEL_ID,
+  INDUSTRY_TYPE_ID: process.env.PAYTM_INDUSTRY_TYPE_ID,
+  ORDER_ID: uuid(),
+  TXN_AMOUNT: "100",
+  CUST_ID: process.env.PAYTM_CUST_ID,
+  CALLBACK_URL: 'http://localhost:8000/callback',
+  EMAIL: 'abhishkbhi@gmail.com',
+  MOBILE_NO: '1234567890',
+};
